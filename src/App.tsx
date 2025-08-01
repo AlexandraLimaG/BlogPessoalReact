a868aee778ed1c69aea13c588b90db63c6662c54
AlexandraLimaG <agdelima321@gmail.com>
2025-08-01 10:54:20 -0300

    adição de postagens

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,10 @@ import Login from './pages/login/Login'
 import Cadastro from './pages/cadastro/Cadastro'
 import FormTema from './components/temas/formtema/FormTema'
 import DeletarTema from './components/temas/deletartema/DeletarTema'
+import ListaPostagens from './components/postagens/listapostagens/ListaPostagens'
+import FormPostagem from './components/postagens/formpostagem/FormPostagem'
+import DeletarPostagem from './components/postagens/deletarpostagem/DeletarPostagem'
+import Perfil from './components/perfil/Perfil'
 
 function App() {
   return (
@@ -26,6 +30,11 @@ function App() {
               <Route path="/cadastrartema" element={<FormTema />} />
               <Route path="/editartema/:id" element={<FormTema />} />
               <Route path="/deletartema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastrarpostagem" element={<FormPostagem />} />
+              <Route path="/editarpostagem/:id" element={<FormPostagem />} />
+              <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+              <Route path="/perfil" element={<Perfil />} />
             </Routes>
           </div>
           <Footer />
